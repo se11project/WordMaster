cdb389b719ec4171fbbba48dcbb541c50d81d684
fuzhiyue84 <fuzhiyue84@gmail.com>
2014-05-30 13:08:05 +0800

    first

---
+++ SE_lab4/src/model/SingleWord.java
@@ -0,0 +1,21 @@
+package model;
+
+public class SingleWord {
+
+	private String content;
+	private String chinese;
+	private int status = 0;
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public String getChinese() {
+		return chinese;
+	}
+	
+	public int getStatus() {
+		return status;
+	}
+	
+}
---
+++ SE_lab4/src/model/WordLibrary.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+
+public class WordLibrary {
+
+	private ArrayList<SingleWord> wordlist;
+	private int LastWordNumber;
+	private String LibraryName;
+	
+	
+	
+}
